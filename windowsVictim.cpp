098fb95c617e0d2321c1076ba2c512b4d9fac669
exercsie <hammondtristan62@gmail.com>
2026-01-28 05:14:39 +1100

    Fixed the socket creation, I forgot to initialise winsock

--- windowsVictim.cpp
+++ windowsVictim.cpp
@@ -9,6 +9,13 @@ int main() {
     WSADATA wsa;
     SOCKET sock = INVALID_SOCKET;
 
+    if(WSAStartup(MAKEWORD(2, 2), &wsa) != 0) { //ver 2.2
+        perror("WSAStartup failed\n");
+        return 1;
+    } else {
+        std::cout << "WSAStartup created\n";
+    }
+
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if(sock == INVALID_SOCKET) {
         perror("Socket failed to create\n");
