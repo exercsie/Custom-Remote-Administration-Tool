e52bb36a817222acf5efaf7d6cacda5cdd217c78
exercsie <lxpx1920@gmail.com>
2026-01-27 09:34:49 -0500

    Added socket arguments and an error message if the socket is not created

--- linuxServer.cpp
+++ linuxServer.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstring>
+#include <sys/socket.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
@@ -7,6 +9,15 @@
 #define PORT 4444
 
 int main() {
+
+    int serverFileDescripter; // 0 = standard input, 1= standard output, 2= standard error
+    char buffer[BUFFERSIZE];
+
+    serverFileDescripter = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverFileDescripter < 0) {
+        perror("socket");
+        return 1;
+    }
 
     return 0;
 }
