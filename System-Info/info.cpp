69353e49e717923cbf7447553d5a31d3c04cdf1e
exercsie <hammondtristan62@gmail.com>
2026-02-20 00:23:35 +1100

    Get number of processors and Architecture

--- System-Info/info.cpp
+++ System-Info/info.cpp
@@ -18,5 +18,18 @@ std::string getSystemInfo() {
         info << SUCCESS_PREFIX << " OS: Windows " << osver.dwMajorVersion << std::endl;
     }
 
+    SYSTEM_INFO sysInfo;
+    GetSystemInfo(&sysInfo);
+    info << SUCCESS_PREFIX << " Client Architecture: ";
+    switch(sysInfo.wProcessorArchitecture) {
+        case PROCESSOR_ARCHITECTURE_AMD64:
+            info << "AMD x64\n";
+            break;
+        case PROCESSOR_ARCHITECTURE_INTEL:
+            info << "INTEL x86\n";
+    }
+
+    info << SUCCESS_PREFIX << " Client has " << sysInfo.dwNumberOfProcessors << " processors\n";
+
     return info.str();
 }
